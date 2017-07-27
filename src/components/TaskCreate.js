8601233a9cfa48c791777600886cbd47d1995dc8
Ake Tangkananond <iamake@gmail.com>
2017-07-27 22:29:53 +0700

    Add state on TaskCreate

--- src/components/TaskCreate.js
+++ src/components/TaskCreate.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-function TaskCreate() {
-  return (
-    <input
-      className="form-control input-lg"
-      type="text"
-      placeholder="What is needed to be done ?"
-    />
-  );
+class TaskCreate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: '' };
+  }
+
+  render() {
+    const { title } = this.state;
+
+    return (
+      <input
+        className="form-control input-lg"
+        type="text"
+        placeholder="What is needed to be done ?"
+        value={title}
+      />
+    );
+  }
 }
 
 export default TaskCreate;
