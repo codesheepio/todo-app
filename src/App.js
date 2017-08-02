ca5ca6d9fb62039221fee1454d4ad6f87e89b4e4
Ake Tangkananond <iamake@gmail.com>
2017-08-02 19:56:53 +0700

    Prepare a workshop

--- src/App.js
+++ src/App.js
@@ -9,7 +9,56 @@ const cx = classNames.bind(styles);
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1 className={cx('text-center', 'heading')}>todos</h1>
+      <div className="row">
+        <div className="col-lg-6 col-lg-push-3 col-sm-8 col-sm-push-2">
+          <input
+            className="form-control input-lg"
+            type="text"
+            placeholder="What is needed to be done ?"
+          />
+          <hr />
+          <ul className="list-group">
+            <li className={cx('list-group-item', 'taskItem')}>
+              <input type="checkbox" className={cx('btnToggle')} />
+              <button className={cx('btn', 'btn-sm', 'btn-link', 'btnRemove')}>✕</button>
+              <div className={cx('title')}>
+                Prepare dinner ingredients
+              </div>
+            </li>
+            <li className={cx('list-group-item', 'taskItem')}>
+              <input type="checkbox" className={cx('btnToggle')}/>
+              <button className={cx('btn', 'btn-sm', 'btn-link', 'btnRemove')}>✕</button>
+              <div className={cx('title')}>
+                Do dishes left over from lastnight
+              </div>
+            </li>
+            <li className={cx('list-group-item', 'taskItem', 'done')}>
+              <input type="checkbox" className={cx('btnToggle')} defaultChecked />
+              <button className={cx('btn', 'btn-sm', 'btn-link', 'btnRemove')}>✕</button>
+              <div className={cx('title')}>
+                Do homework
+              </div>
+            </li>
+          </ul>
+          <hr />
+          <div className="row">
+            <div className="col-sm-3 text-center">
+              <button className="btn btn-sm btn-link">Clear done</button>
+            </div>
+            <div className="col-sm-6 text-center">
+              <ul className={cx('pagination', 'pagination-sm', 'taskFilter')}>
+                <li className="active"><span>All</span></li>
+                <li><span>Active</span></li>
+                <li><span>Done</span></li>
+              </ul>
+            </div>
+            <div className="col-sm-3 text-center hidden-xs">
+              <span className={cx('text-muted', 'small', 'activeCount')}>2 items left</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
